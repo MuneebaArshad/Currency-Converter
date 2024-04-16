ec1a6acf80a7a72c6afd9030ca5677c1d42df279
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-04-17 02:41:03 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,49 @@
+#! /usr/bin/env node 
+
+
+ import inquirer from "inquirer"
+
+
+ const currency:any = {
+
+    USD: 1, //BASE CURRENCY
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+
+ }
+
+ let userAns = await inquirer.prompt([
+{
+    name:"from",
+    type:"list",
+    message:"Enter from currency",
+    choices:["USD","EUR","GBP","INR","PKR"]
+},
+{
+    name:"to",
+    type:"list",
+    message:"Enter to currency",
+    choices:["USD","EUR","GBP","INR","PKR"]
+},
+{
+    name:"amount",
+    type:"number",
+    message:"Enter Your Amount",
+     
+}
+])
+//Dynamic approach
+
+
+let fromAmount = currency[userAns.from]   //exchange rate
+let toAmount = currency[userAns.to]         //exchange rate
+let  Amount =  userAns.amount 
+let baseAmount = Amount  / fromAmount   //use base curency
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount)
+ 
+
+
+ 
